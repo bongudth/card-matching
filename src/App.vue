b7405292f4d7da83cd490fb2d60013e868af5c84
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-07-05 09:53:04 +0700

    ⚙️ fix: allow user to see incorrect match before hiding

--- src/App.vue
+++ src/App.vue
@@ -50,42 +50,42 @@ export default {
     };
 
     const restartGame = () => {
-      shuffleCards()
-      
+      shuffleCards();
+
       cardList.value = cardList.value.map((card, index) => {
         return {
           ...card,
           matched: false,
           position: index,
-          visible: false
-        }
-      })
-    }
+          visible: false,
+        };
+      });
+    };
 
-    const cardItems = [1, 2, 3, 4, 5, 6, 7, 8]
-    
-    cardItems.forEach(item => {
+    const cardItems = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    cardItems.forEach((item) => {
       cardList.value.push({
         value: item,
-        visible: true,
-        position: null,
-        matched: false
-      })
-
-      cardList.value.push({
-        value: item,
-        visible: true,
+        visible: false,
         position: null,
         matched: false,
-      })
-    })
+      });
+
+      cardList.value.push({
+        value: item,
+        visible: false,
+        position: null,
+        matched: false,
+      });
+    });
 
     cardList.value = cardList.value.map((card, index) => {
       return {
         ...card,
-        position: index
-      }
-    })
+        position: index,
+      };
+    });
 
     const flipCard = (payload) => {
       cardList.value[payload.position].visible = true;
@@ -108,8 +108,10 @@ export default {
             cardList.value[cardOne.position].matched = true;
             cardList.value[cardTwo.position].matched = true;
           } else {
-            cardList.value[cardOne.position].visible = false;
-            cardList.value[cardTwo.position].visible = false;
+            setTimeout(() => {
+              cardList.value[cardOne.position].visible = false;
+              cardList.value[cardTwo.position].visible = false;
+            }, 2000)
           }
 
           userSelection.value.length = 0;
@@ -124,7 +126,7 @@ export default {
       userSelection,
       status,
       shuffleCards,
-      restartGame
+      restartGame,
     };
   },
 };
